0594a891e79aac51c16596857640f0ab93bc60aa
Kyaw-Ye-Naing <kyawyenaing771619@gmail.com>
2023-10-20 16:59:13 +0630

    static link in utility

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use('/quality', express.static(path.join(__dirname, 'aq-wq-web')));
 // app.use('/utility', express.static(path.join(__dirname, 'ads-app')));
 // app.use('/farm', express.static(path.join(__dirname, 'vertical-farm-app')));
 app.use('/ml', express.static(path.join(__dirname, 'ml-web')));
+app.use('/adsv2', express.static(path.join(__dirname, 'utility-web-v2')));
 
 // const BASE_URL = "https://adsapi.emscloud.net";
 // const BASE_URL='http://206.189.39.45:8080'
@@ -24,6 +25,10 @@ app.use('/ml', express.static(path.join(__dirname, 'ml-web')));
 
  app.get('/co2/*',cors(), function(req, res) {
   res.sendFile(path.join(__dirname,'co2-web','index.html'));
+});
+
+app.get('/adsv2/*',cors(), function(req, res) {
+  res.sendFile(path.join(__dirname,'utility-web-v2','index.html'));
 });
 
  app.get('/ml/*',cors(), function(req, res) {
